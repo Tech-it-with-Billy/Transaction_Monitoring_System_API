06cf7f51ef0fba5acf8c3f53fe408dc58767268a
Billy Och <ochiengbilly02@gmail.com>
2025-10-31 07:08:26 +0300

    Update: Customizing views using permission classes, and updating api endpoints.

--- User/urls.py
+++ User/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ProfileView, RegistrationView
+from .views import ProfileView, RegistrationView, AdminUserDetailView, UserListView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
@@ -8,4 +8,6 @@ urlpatterns = [
     path('api/auth/login/', TokenObtainPairView.as_view(), name='login'),
     path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/user/profile/', ProfileView.as_view(), name='profile'),
+    path('api/users/', UserListView.as_view(), name='analyst'),
+    path('api/user/admin/', AdminUserDetailView.as_view(), name='admin')
 ]
--- User/views.py
+++ User/views.py
@@ -1,15 +1,27 @@
 from rest_framework import generics, permissions
 from .models import CustomUser
 from .serializers import RegistrationSerializer, ProfileSerializer
-
+from .permissions import IsAdmin
 class RegistrationView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = RegistrationSerializer
+    permission_classes = [permissions.AllowAny]
 
-class ProfileView(generics.RetrieveUpdateDestroyAPIView):
+class ProfileView(generics.RetrieveUpdateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = ProfileSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, permissions.AllowAny]
     
     def get_object(self):
         return self.request.user
+
+class AdminUserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [IsAdmin]
+
+class UserListView(generics.ListAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [permissions.IsAuthenticated, IsAdmin]
+
